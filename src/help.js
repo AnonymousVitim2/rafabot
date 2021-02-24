a5d1f71b2ac8f2fd8070a50dab9d5002efc7e31b
AnonymousVitim2 <lobootff3@gmail.com>
2021-02-24 11:03:36 -0400

    First commit

---
+++ src/help.js
@@ -0,0 +1,24 @@
+const help = (prefix) => { 
+	return `
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬
+▣═─⊱【 𝑴𝑬𝑵𝑼 𝑺𝑰𝑴𝑷𝑳𝑬 】⊰─══
+║
+║|─⊱ *${prefix}menu1 (ownermenu)*
+║|─⊱ *${prefix}menu2 (adminmenu)*
+║|─⊱ *${prefix}menu3 (nsfwmenu)*
+║|─⊱ *${prefix}menu4 (funmenu)*
+║|─⊱ *${prefix}menu5 (mediamenu)*
+║|─⊱ *${prefix}menu6 (makermenu)*
+║|─⊱ *${prefix}menu7 (vipmenu [VIP ESPECIAL!!])*
+║|─⊱ *${prefix}menu8 (kerangmenu)*
+║|─⊱ *${prefix}menu9 (animemenu)*
+║|─⊱ *${prefix}menu10 (othermenu)*
+║|─⊱ *${prefix}menu11 (othermenu2)*
+║
+╰─⊱ *ꜱᴛᴀᴛᴜꜱ ᴅᴏ ʙᴏᴛ: Online*
+║
+▣══─【 ᭕̸᬴͜͡🌹𝗥𝗮𝗽𝗵𝗮᭄ঔৣ͡𝐇𝐘͢𝐏𝜩₃₂₃🌹シ 】─══▣
+▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬
+`
+}
+exports.help = help
